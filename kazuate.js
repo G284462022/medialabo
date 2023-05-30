f8345363deb34d1dd8f733f2aa3fcba0c1343dfb
G284462022 <228446@st.takushoku-u.ac.jp>
2023-05-30 16:56:45 +0900

    課題

--- kazuate.js
+++ kazuate.js
@@ -33,7 +33,8 @@ function hantei(){
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-
+  let hit = document.querySelector("p#hitokoto");
+  hit.textContent = "【判定結果】";
   let seigo = {
     seikai: "正解です．おめでとう!",
     tiisai: "まちがい．答えはもっと大きいですよ",
